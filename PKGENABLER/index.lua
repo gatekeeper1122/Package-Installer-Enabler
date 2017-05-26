06b1e15af403a39e3f4ce5069100ae96ce9974b4
Silica <earsyum@gmail.com>
2017-05-27 00:53:19 +1200

    Add files via upload

--- PKGENABLER/index.lua
+++ PKGENABLER/index.lua
@@ -3,7 +3,28 @@
         System.launchEboot("app0:/VitaRW_for_LUA.bin")
     else
         System.deleteFile("ux0:/test.txt")
-        System.extractZIP("app0:/pkgpatch.zip", "vs0:/app/NPXS10031")
-        System.launchEboot("app0:/patch_app_db.bin")
+        -- Create a new color
+    white = Color.new(255,255,255,255) 
     end
     
+while true do
+	
+	Graphics.initBlend()
+	Screen.clear()
+	Graphics.debugPrint(5, 5, "PKGEnabler By SilicaAndPina!", white)
+    Graphics.debugPrint(5, 25, "Which PKG Installer Mod Would You Like To Use?", white)
+    Graphics.debugPrint(5, 45, "X: DEX PKG INSTALLER (The PKG Installer you'd get on a TESTKIT PSV)", white)
+    Graphics.debugPrint(5, 65, "O: MAJOR_TOM PKG INSTALLER (The PKG Installer in IDU Mode)", white)
+	Graphics.termBlend()
+	Screen.flip()
+	
+	if Controls.check(Controls.read(), SCE_CTRL_CROSS) then
+        System.extractZIP("app0:/pkgpatchDEX.zip", "vs0:/app/NPXS10031")
+        System.launchEboot("app0:/patch_app_db.bin")
+    else if Controls.check(Controls.read(), SCE_CTRL_CIRCLE) then
+        System.extractZIP("app0:/pkgpatchMAJOR.zip", "vs0:/app/NPXS10031")
+        System.launchEboot("app0:/patch_app_db.bin")
+	end
+	
+end
+end
